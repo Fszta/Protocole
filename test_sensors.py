34d6491f66eef276b95a280524175101cd62c471
Fszta <36471574+Fszta@users.noreply.github.com>
2018-09-10 09:38:28 +0000

    Ajout test capteurs

---
+++ test_sensors.py
@@ -0,0 +1,45 @@
+# coding: utf-8
+__author__='Ferszterowski Antoine, antoinefer@hotmail.com'
+
+import time
+from  get_value_sensor import PulseSensor
+from i2c_gsr_from_arduino import GsrSensor
+import matplotlib.pyplot as plt
+
+def Programm():
+    test_sensor = TestSensor()
+    test_sensor.run()
+
+
+class TestSensor():
+
+    """ Code pour tester le bon positionnement du capteur sur le doigt"""
+    def __init__(self):
+        self.pulse_sensor = PulseSensor()
+        self.gsr_sensor = GsrSensor()
+        self.time_init = time.time()
+
+    def run(self):
+        """Récupère les valeurs du capteur après 15 secondes"""
+        print("Debut capture...")
+        self.pulse_sensor.start()
+        self.gsr_sensor.start()
+
+        while True:
+            time.sleep(1)
+            if time.time() - self.time_init > 15:
+                self.pulse_sensor.stop_capture()
+                self.gsr_sensor.stop_capture()
+
+                self.value_pulse, self.time_pulse, _ = self.pulse_sensor.get_value()
+                self.value_gsr, self.time_gsr, _= self.gsr_sensor.get_value()
+
+                print("Fin capture...")
+                plt.plot(self.time_pulse,self.value_pulse)
+                plt.show()
+                plt.plot(self.time_gsr, self.value_gsr)
+                plt.show()
+                break
+
+if __name__ == '__main__':
+    Programm()
