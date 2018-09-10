882fbe6d38686485d31514ed4e3ecfd4320ca5f4
Fszta <36471574+Fszta@users.noreply.github.com>
2018-09-10 10:01:18 +0000

    Add files via upload

--- test_record.py
+++ test_record.py
@@ -1,30 +1,23 @@
 # coding: utf-8
 __author__ = 'Ferszterowski Antoine, antoinefer@hotmail.com'
 
-import os
-from threading import Thread
-import argparse
 from picamera import PiCamera
 from time import sleep
-import RPi.GPIO as GPIO
+
 
 def Programm():
     camera = PiCamera()
     camera.rotation = 180
  
     camera.start_preview()
+    
+    # Affiche pendant 500 secondes
     sleep(500)
 
-    # Fin de l'enregistrement
-    
-    
     camera.stop_preview()
-
-
-    self.camera.close()
+    camera.close()
     print("Fin de l'enregistrement")
 
-    
     
 if __name__ == '__main__':
     Programm()    
