167ec55b73df7ffd2d9632107dd9955b872f5a4f
Fszta <36471574+Fszta@users.noreply.github.com>
2018-09-10 10:16:40 +0000

    Add files via upload

--- ppg_sensor.py
+++ ppg_sensor.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 import time
 import sys
 from threading import Thread
@@ -19,24 +20,23 @@ class PulseSensor (Thread):
     def run(self):
         """Boucle infinie, capture"""
         while not self.stopped :
-            if len(self.signal_sensor) >  6500:
-                self.signal_sensor = self.signal_sensor[len(self.signal_sensor)-6500:]
-                self.time = self.time[len(self.time)-6500:]
+            
             signal = self.adc.read(self.channel)
             self.signal_sensor.append(signal)
             self.time.append(time.time() - self.time_start)
             time.sleep(1./200)
-            #print(signal)
+
 
     def get_value(self):
-
+        """Calcul de f_sampling, filtrage du signal par passe bande"""
         fs = int(len(self.signal_sensor)/(time.time() - self.time_start))
-        filtered_signal = filtrage.run(self.signal_sensor,self.time_start,fs,0.8,4,4)
-        #filtered_signal = self.signal_sensor
+        filtered_signal = filtrage.run(self.signal_sensor,self.time_start,fs,0.7,4,4)
+
         return filtered_signal, self.time, fs
 
 
     def reset_signal(self):
+        """Réinitialisation du signal"""
         print("Reset signal capteur...")
         self.signal_sensor = []
         self.time = []
